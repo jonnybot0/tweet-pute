bb9e06d5213ee8268620bbd367920621b426df41
Jonny Carter <jonteh1337@gmail.com>
2015-05-15 23:33:58 -0500

    Quick script sample

---
+++ TweetPuteScript.groovy
@@ -0,0 +1,37 @@
+@Grab(group='org.codehaus.gpars', module='gpars', version='1.2.1')
+@Grab(group="org.twitter4j",      module="twitter4j-core", version="4.0.3")
+@Grab(group="org.twitter4j",      module="twitter4j-stream", version="4.0.3")
+import groovyx.gpars.*
+import twitter4j.*
+
+def stream = new TwitterStreamFactory().getInstance()
+
+def listener = new StatusListener() {
+    @Override
+    void onStatus(Status status) {
+        System.out.println(status.getUser().getName() + " @ " + status.getCreatedAt() + " : " + status.getText())
+    }
+ 
+    @Override
+    void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) { }
+ 
+    @Override
+    void onTrackLimitationNotice(int i) { }
+ 
+    @Override
+    void onScrubGeo(long l, long l2) { }
+ 
+    @Override
+    void onStallWarning(StallWarning stallWarning) { }
+ 
+    @Override
+    void onException(Exception e) {
+        e.printStackTrace()
+    }
+}
+def filter = new FilterQuery()
+String[] query = ["Liverpool", "Football"]
+filter.track(query)
+ 
+stream.addListener(listener)
+stream.filter(filter)
---
+++ src/main/groovy/TweetFeeder.groovy
@@ -0,0 +1,6 @@
+/**
+ * Created by jonny on 5/15/15.
+ */
+class TweetFeeder {
+
+}
